879d04c8ec3c3ede110545800e4cdf75abc2522c
mehardiknaik <hardiknaik7444@gmail.com>
2025-05-31 15:08:05 +0530

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import style from "./App.module.css";
-import webpackLogo from "./assets/webpack.png"
+import webpackLogo from "./assets/webpack.png";
 
 const App = () => {
-  return <div className={style.container}>
-    <div>React Webpack App</div>
-    <div className={style.logoContainer}>
-    <img src="./react.svg" alt="React Logo" className={style.react} />
-    <img src={webpackLogo} alt="Webpack Logo" className={style.webpack} />
+  return (
+    <div className={style.container}>
+      <div>React Webpack App</div>
+      <div className={style.logoContainer}>
+        <img src="./react.svg" alt="React Logo" className={style.react} />
+        <img src={webpackLogo} alt="Webpack Logo" className={style.webpack} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default App;
