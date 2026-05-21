3043e9e5389aab9518bad36f1d9f861d7322cbdc
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2026-05-21 22:26:21 +0530

    fix suspense hoc

--- src/HOC/withSuspense.tsx
+++ src/HOC/withSuspense.tsx
@@ -5,11 +5,21 @@ function withSuspense<T extends object>(
   LazyComponent: React.LazyExoticComponent<ComponentType<T>>,
   fallback: ReactNode = <div>Loading...</div>
 ): React.FC<T> {
-  return (props: T) => (
+  const SuspendedComponent = (props: T) => (
     <Suspense fallback={fallback}>
       <LazyComponent {...props} />
     </Suspense>
   );
+
+  const lazyComponentWithMetadata = LazyComponent as unknown as {
+    displayName?: string;
+    name?: string;
+  };
+  const displayName =
+    lazyComponentWithMetadata.displayName || lazyComponentWithMetadata.name || 'Component';
+  SuspendedComponent.displayName = `WithSuspense(${displayName})`;
+
+  return SuspendedComponent;
 }
 
 export default withSuspense;
